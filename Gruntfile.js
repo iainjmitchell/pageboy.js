5f80c534a0ffdca80b6d73e148c53f0bc2c73c65
iainjmitchell <iainjmitchell@gmail.com>
2014-01-06 20:34:25 +0000

    no commit message :(

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+	var message = grunt.option('message') || 'no commit message :(';
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+			files: ["./**/*.js"],
+			options: {
+				ignores: ["./node_modules/**/*.js"]
+			}
+		},
+		gitcommit: {
+			your_target: {
+				options:{
+					message: message
+				},
+				src: ['.']
+			}
+		}, 
+		shell: {
+			git_push: {
+				command: 'git push'
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-git');
+	grunt.loadNpmTasks('grunt-shell');
+
+	grunt.registerTask('test', ['jshint']);
+	grunt.registerTask('default',['test','gitcommit', 'shell:git_push']);
+};
