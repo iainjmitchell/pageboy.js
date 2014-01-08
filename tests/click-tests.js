c198bea5b82f6f05bc802bbede29873538c9ee4c
iainjmitchell <iainjmitchell@gmail.com>
2014-01-08 09:40:30 +0000

    refactoring

--- tests/click-tests.js
+++ tests/click-tests.js
@@ -5,35 +5,37 @@ describe("Given that developer wants to simulate the click of an item on a a pag
 		document.body.innerHTML = TEST_FIXTURE_HTML;
 	});
 
-	describe("When click link using an existing anchor's id", function(){
-		it("Then link is clicked", function(done){
-			var EXISTING_LINK_ID = 'existingLink';
-			$('#' + EXISTING_LINK_ID).click(function(){
+	describe("When click link is called", function(){
+		describe("using an existing anchor's id", function(){
+			it("Then link is clicked", function(done){
+				var EXISTING_LINK_ID = 'existingLink';
+				$('#' + EXISTING_LINK_ID).click(function(){
+					done();
+				});
+				clickLink(EXISTING_LINK_ID);
+			});
+		});
+
+		describe("using an existing button's id", function(){
+			it("Then button is NOT clicked", function(done){
+				var EXISTING_BUTTON_ID = 'existingButton';
+				$('#' + EXISTING_BUTTON_ID).click(function(){
+					done('Button should NOT have been clicked');
+				});
+				clickLink(EXISTING_BUTTON_ID);
 				done();
 			});
-			clickLink(EXISTING_LINK_ID);
 		});
-	});
 
-	describe("When click link using an existing button's id", function(){
-		it("Then button is NOT clicked", function(done){
-			var EXISTING_BUTTON_ID = 'existingButton';
-			$('#' + EXISTING_BUTTON_ID).click(function(){
-				done('Button should NOT have been clicked');
+		describe("using an existing anchor's link text", function(){
+			it("Then link is clicked", function(done){
+				var EXISTING_LINK_ID = 'existingLink',
+					EXISTING_LINK_TEXT = 'a link';
+				$('#' + EXISTING_LINK_ID).click(function(){
+					done();
+				});
+				clickLink(EXISTING_LINK_TEXT);
 			});
-			clickLink(EXISTING_BUTTON_ID);
-			done();
-		});
-	});
-
-	describe("When click link using an existing anchor's link text", function(){
-		it("Then link is clicked", function(done){
-			var EXISTING_LINK_ID = 'existingLink',
-				EXISTING_LINK_TEXT = 'a link';
-			$('#' + EXISTING_LINK_ID).click(function(){
-				done();
-			});
-			clickLink(EXISTING_LINK_TEXT);
 		});
 	});
 });
