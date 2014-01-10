c55707f3314e2cec5b874f90bf468371cfab5fee
iainjmitchell <iainjmitchell@gmail.com>
2014-01-10 16:36:53 +0000

    refactoring

--- src/pageboy.js
+++ src/pageboy.js
@@ -1,9 +1,9 @@
 (function($, module){
 	var Pageboy = function(context){
 		var contextElement = $(context),
-			links = new LinkRepository(contextElement),
-			buttons = new ButtonRepository(contextElement),
-			textElements = new TextElementRepository(contextElement);
+			links = new click.LinkRepository(contextElement),
+			buttons = new click.ButtonRepository(contextElement),
+			textElements = new text.TextElementRepository(contextElement);
 
 		this.clickLink = function (linkIdOrText){
 			links.get(linkIdOrText).click();
@@ -21,51 +21,97 @@
 		};
 	};
 
-	var TextElementRepository = function(contextElement){
-		this.get = function(textElementIdOrLabel){
-			var selector = getSelector(textElementIdOrLabel, contextElement);
-			return new TextElement(contextElement, selector);
+	var click = (function(){
+		var LinkRepository = function(context){
+			var linkSelectorFactory = new IdOrTextSelectorFactory('a'),
+				clickableElementFactory = new ClickableElementFactory(context, linkSelectorFactory);
+
+			this.get = clickableElementFactory.create;
 		};
 
-		function getSelector(id, contextElement){
-			var labelForElement = contextElement.find('label:contains('+ id +')').attr('for');
-			var labelSelector = labelForElement ? ',#' + labelForElement : '';
-			var selector = '#' + id + labelSelector;
-			return selector;
-		}
-	};
+		var ButtonRepository = function(context){
+			var buttonSelectorFactory = new MultipleSelectorFactory([
+					new IdOrTextSelectorFactory('button'),
+					new IdOrValueSelectorFactory('input[type=button]')
+				]),
+				clickableElementFactory = new ClickableElementFactory(context, buttonSelectorFactory);
 
-	var TextElement = function(context, selector){
-		var element = context.find(selector);
-
-		this.fillIn = function(value){
-			element.val(value);
+			this.get = clickableElementFactory.create;
 		};
+
+		var ClickableElementFactory = function(context, selectorFactory){
+			this.create = function(elementQuery){
+				var selector = selectorFactory.create(elementQuery);
+				return new ClickableElement(context, selector);
+			};
+		};
+
+		var ClickableElement = function(context, selector){
+			var element = context.find(selector);
+
+			this.click = function(){
+				element.click();
+			};
+		};
+
+		return {
+			LinkRepository : LinkRepository,
+			ButtonRepository : ButtonRepository
+		};
+	})();
+
+	var text = (function(){
+		var TextElementRepository = function(contextElement){
+			var idOrLabelForSelectorFactory = new IdOrLabelForSelectorFactory(contextElement, 'input[type=text]');
+			this.get = function(textElementIdOrLabel){
+				var selector = idOrLabelForSelectorFactory.create(textElementIdOrLabel);
+				return new TextElement(contextElement, selector);
+			};
+		};
+
+		var TextElement = function(context, selector){
+			var element = context.find(selector);
+
+			this.fillIn = function(value){
+				element.val(value);
+			};
+		};
+
+		return {
+			TextElementRepository : TextElementRepository
+		};
+	})();
+
+	var IdOrLabelForSelectorFactory = function(contextElement, elementType){
+		var multipleSelectorFactory = new MultipleSelectorFactory([
+				new ElementByIdSelectorFactory(elementType),
+				new ElementByLabelForSelectorFactory(contextElement, elementType)
+			]);
+		this.create = multipleSelectorFactory.create;
 	};
 
-	var ClickableElement = function(context, selector){
-		var element = context.find(selector);
-
-		this.click = function(){
-			element.click();
+	var ElementByLabelForSelectorFactory = function(contextElement, elementType){
+		this.create = function(elementQuery){
+			var labelForElementId = contextElement.find('label:contains('+ elementQuery +')').attr('for');
+			if (labelForElementId){
+				return elementType + '#' + labelForElementId;
+			}
 		};
 	};
 
 	var IdOrTextSelectorFactory = function(elementType){
-		var selectorFactories = [
+		var multipleSelectorFactory = new MultipleSelectorFactory([
 				new ElementByIdSelectorFactory(elementType),
 				new ElementByTextSelectorFactory(elementType)
-			],
-			multipleSelectorFactory = new MultipleSelectorFactory(selectorFactories);
+			]);
 		this.create = multipleSelectorFactory.create;
 	};
 
 	var IdOrValueSelectorFactory = function(elementType){
-		var selectorFactories = [
+		var multipleSelectorFactory = new MultipleSelectorFactory([
 				new ElementByIdSelectorFactory(elementType),
 				new ElementByValueSelectorFactory(elementType)
-			],
-			multipleSelectorFactory = new MultipleSelectorFactory(selectorFactories);
+			]);
 		this.create = multipleSelectorFactory.create;
 	};
 
@@ -73,53 +119,36 @@
 		this.create = function(elementQuery){
 			var selectors = [];
 			$.each(selectorFactories, function(){
-				selectors.push(this.create(elementQuery));
+				var selector = this.create(elementQuery);
+				if (selector){
+					selectors.push(selector);
+				}
 			});
 			return selectors.join(', ');
 		};
 	};
 
 	var ElementByIdSelectorFactory = function(elementType){
-		this.create = function(elementIdOrText){
-			return elementType + '#' + elementIdOrText;
+		this.create = function(elementQuery){
+			return elementType + '#' + elementQuery;
 		};
 	};
 
 	var ElementByTextSelectorFactory = function(elementType){
-		this.create = function(elementIdOrText){
-			return elementType + ':contains(' + elementIdOrText + ')';
+		this.create = function(elementQuery){
+			return elementType + ':contains(' + elementQuery + ')';
 		};
 	};
 
 	var ElementByValueSelectorFactory = function(elementType){
-		this.create = function(elementIdOrText){
-			return elementType + '[value="' + elementIdOrText + '"]';
-		};
-	};
-
-	var LinkRepository = function(context){
-		var linkSelectorFactory = new IdOrTextSelectorFactory('a'),
-			clickableElementFactory = new ClickableElementFactory(context, linkSelectorFactory);
-
-		this.get = clickableElementFactory.create;
-	};
-
-	var ButtonRepository = function(context){
-		var buttonSelectorFactory = new MultipleSelectorFactory([
-				new IdOrTextSelectorFactory('button'),
-				new IdOrValueSelectorFactory('input[type=button]')
-			]),
-			clickableElementFactory = new ClickableElementFactory(context, buttonSelectorFactory);
-
-		this.get = clickableElementFactory.create;
-	};
-
-	var ClickableElementFactory = function(context, selectorFactory){
 		this.create = function(elementQuery){
-			var selector = selectorFactory.create(elementQuery);
-			return new ClickableElement(context, selector);
+			return elementType + '[value="' + elementQuery + '"]';
 		};
 	};
+
+	
+
+	
 
 	(function exposeDSL(module, Pageboy){
 		var pageboy = new Pageboy(document);
