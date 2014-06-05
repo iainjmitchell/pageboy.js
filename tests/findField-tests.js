6d96df141bd530775b4b70732f023f774ee9e75e
iainjmitchell <iainjmitchell@gmail.com>
2014-06-05 20:29:51 +0100

    refactoring

--- tests/findField-tests.js
+++ tests/findField-tests.js
@@ -6,22 +6,24 @@ describe("Given that developer wants to find a field on a page", function(){
 	});
 
 	describe('When findField is called', function(){
-		describe('And field exists with matching id', function(){
-			it('Then text inputs value is returned', function(){
-				var TEXT_INPUT_VALUE = 'random value ' + Math.random(),
-					EXISTING_TEXT_INPUTS_ID = 'existingTextInput';
-				$('#' + EXISTING_TEXT_INPUTS_ID).val(TEXT_INPUT_VALUE);
-				findField(EXISTING_TEXT_INPUTS_ID).value.should.equal(TEXT_INPUT_VALUE);
-			});
-		});
+		describe('against a existing text input', function(){
+			var EXISTING_TEXT_INPUTS_ID = 'existingTextInput';
 
-		describe('And field exists with matching label for', function(){
-			it('Then text inputs value is returned', function(){
-				var TEXT_INPUT_VALUE = 'random value ' + Math.random(),
-					EXISTING_TEXT_INPUTS_LABEL_FOR = 'Existing text input label',
-					EXISTING_TEXT_INPUTS_ID = 'existingTextInput';
-				$('#' + EXISTING_TEXT_INPUTS_ID).val(TEXT_INPUT_VALUE);
-				findField(EXISTING_TEXT_INPUTS_LABEL_FOR).value.should.equal(TEXT_INPUT_VALUE);
+			describe("by using it's id", function(){
+				it('Then text inputs value is returned', function(){
+					var TEXT_INPUT_VALUE = 'random value ' + Math.random();
+					$('#' + EXISTING_TEXT_INPUTS_ID).val(TEXT_INPUT_VALUE);
+					findField(EXISTING_TEXT_INPUTS_ID).value.should.equal(TEXT_INPUT_VALUE);
+				});
+			});
+
+			describe("by using it's label for", function(){
+				it('Then text inputs value is returned', function(){
+					var TEXT_INPUT_VALUE = 'random value ' + Math.random(),
+						EXISTING_TEXT_INPUTS_LABEL_FOR = 'Existing text input label';
+					$('#' + EXISTING_TEXT_INPUTS_ID).val(TEXT_INPUT_VALUE);
+					findField(EXISTING_TEXT_INPUTS_LABEL_FOR).value.should.equal(TEXT_INPUT_VALUE);
+				});
 			});
 		});
 	});
